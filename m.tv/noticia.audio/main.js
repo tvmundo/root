f3b9575a47a30d470c4555783ec492a76f2d49bf
tvmundo <43994174+tvmundo@users.noreply.github.com>
2022-05-14 12:11:41 -0300

    Add files via upload

--- m.tv/noticia.audio/main.js
+++ m.tv/noticia.audio/main.js
@@ -22,7 +22,7 @@ let curr_track = document.createElement('audio');
 // Define the tracks that have to be played
 let track_list = [
   {
-name: "Vi crimes abomináveis do batalhão Azov  Azov: ",
+name: "Vi crimes abomináveis do batalhão Azov: ",
     artist: " IMG twitter",
     image: "https://pbs.twimg.com/media/FSf-pdgX0AEpLxJ?format=jpg&name=small",
     path: "https://meupixtv.github.io/br/xxx/2022/mai/14/Vi%20crimes%20abomináveis%20de%20Azov.mp3"
