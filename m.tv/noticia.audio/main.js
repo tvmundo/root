0021e7980b5653a78aa5e26698bd13938349627c
tvmundo <43994174+tvmundo@users.noreply.github.com>
2022-05-26 10:25:02 -0300

    Add files via upload

--- m.tv/noticia.audio/main.js
+++ m.tv/noticia.audio/main.js
@@ -30,13 +30,13 @@ name: "Forte terremoto de magnitude 6,9 ​​sacode o sul do Peru e o noroeste 
   {
 name: "Putin: Nenhum gendarme mundial será capaz de parar os países do mundo que querem desenvolver uma política independente.",
     artist: " IMG twitter",
-    image: "https://pbs.twimg.com/card_img/1529601951087861760/qAya_7kd?format=jpg&name=small",
+    image: "https://pbs.twimg.com/media/FMNv2YWWYAcKHXo?format=jpg&name=small",
     path: "https://meupixtv.github.io/br/xxx/2022/mai/26/Putin_%20_Nenhum%20gendarme%20mundial%20será%20capaz%20de%20para.mp3"
   },
   {
 name: "Agentes da PRF jogam bomba de gás e matam homem asfixiado dentro de viatura em Sergipe.",
     artist: " IMG twitter",
-    image: "https://pbs.twimg.com/media/FMNv2YWWYAcKHXo?format=jpg&name=small",
+    image: "https://pbs.twimg.com/card_img/1529601951087861760/qAya_7kd?format=jpg&name=small",
     path: "https://meupixtv.github.io/br/xxx/2022/mai/26/Agentes%20da%20PRF%20jogam%20bomba%20de%20gás%20e%20matam%20homem%20asfixiado%20dentro%20de%20viatura%20em%20Sergipe.mp3"
   },
   {
